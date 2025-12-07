ad9ec91279ad1df64d0303d747d4d7b98db6f3af
sellitcom <139056459+sellitcom@users.noreply.github.com>
2025-12-07 10:22:54 +0500

    Full Upload 4

--- assets/js/app1.js
+++ assets/js/app1.js
@@ -1,6 +1,10 @@
 function createAccoutForm() {
 	document.querySelector(".signup-screen").style.marginTop = "16vh";
 }
+function signUpGoBack() {
+	document.querySelector(".signup-screen").style.marginTop =
+		"calc(100vh + 50px)";
+}
 function loginForm() {
 	document.querySelector(".login-screen").style.marginTop = "16vh";
 }
@@ -10,5 +14,24 @@ function removeBlueScreen() {
 	setTimeout(() => {
 		let screen = document.querySelector(".blue-screen");
 		screen.remove();
+		createAccoutForm();
 	}, 500);
+}
+function changeGender(gender) {
+	let currentGender = document.querySelector("#selectedGender").innerHTML;
+	if (currentGender == gender) {
+		// do nothing
+	} else {
+		document.querySelector("#selectedGender").innerHTML = gender;
+		document.querySelector("#" + gender + "GenderField").style.border =
+			"1px solid var(--green-2)";
+		if (gender == "male") {
+			oppositeGender = "female";
+		} else {
+			oppositeGender = "male";
+		}
+		document.querySelector(
+			"#" + oppositeGender + "GenderField"
+		).style.border = "none";
+	}
 }
